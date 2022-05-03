34f517254609e16de786775bf2c32807273e4c03
Luke-Young20 <lukegyoung1@gmail.com>
2022-05-03 10:56:18 +0100

    removed old routes

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\FullCalendarEventMasterController;
-use App\Http\Controllers\MainController;
 use App\Http\Controllers\FullCalendarController;
-use App\Http\Controllers\FullCalendarController1;
 
 
 /*
@@ -32,22 +29,6 @@ Route::get('rooms', function () {
 
 require __DIR__.'/auth.php';
 
-//fullcalender
-//Route::get('fullcalender', [FullCalenderController::class, 'index']);
-//Route::post('fullcalenderAjax', [FullCalenderController::class, 'ajax']);
-
-//Route::get('/fullcalendareventmaster', [FullCalendarEventMasterController::class, 'index']);
-//Route::post('/fullcalendareventmaster/create',[FullCalendarEventMasterController::class, 'create']);
-//Route::post('/fullcalendareventmaster/update',[FullCalendarEventMasterController::class, 'update']);
-//Route::post('/fullcalendareventmaster/delete',[FullCalendarEventMasterController::class, 'destroy']);
-
 Route::get('fullcalendar/{id}', [FullCalendarController::class, 'index'])->middleware(['auth']);
 
 Route::post('fullcalendar/action', [FullCalendarController::class, 'action']); 
-
-
-
-Route::get('/main', [MainController::class, 'index']);
-Route::post('/main/checklogin', [MainController::class, 'checklogin']);
-Route::get('main/successlogin', [MainController::class, 'successlogin']);
-Route::get('main/logout', [MainController::class, 'logout']);
